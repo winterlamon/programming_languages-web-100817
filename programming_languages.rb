8fb38e5fd8f2dc39bcb8b2c1ad9deeca4364e82b
Winter LaMon <winter.lamon@gmail.com>
2017-10-06 19:06:12 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -56,8 +56,10 @@ def reformat_languages(languages)
     hash.collect do |language, language_info|
 
       new_hash = hash[language]
-      language_info[:style] = style
-
+      language_info[:style] = style.to_a
+      if style == nil
+        language_info[:style] << style
+      end
 
       binding.pry
 =begin
