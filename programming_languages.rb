2ed2d06e8de02c477da47a43e7468c29f72b8dae
Winter LaMon <winter.lamon@gmail.com>
2017-10-06 18:47:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,74 @@
+require 'pry'
+
+=begin
+
+ #### WHAT languages is at start ####
+
+ languages
+ => {:oo=>
+   {:ruby=>{:type=>"interpreted"},
+    :javascript=>{:type=>"interpreted"},
+    :python=>{:type=>"interpreted"},
+    :java=>{:type=>"compiled"}},
+  :functional=>
+   {:clojure=>{:type=>"compiled"},
+    :erlang=>{:type=>"compiled"},
+    :scala=>{:type=>"compiled"},
+    :javascript=>{:type=>"interpreted"}}}
+
+ #### WHAT we want languages to look like ####
+
+ {
+   :ruby => {
+     :type => "interpreted",
+     :style => [:oo]
+   },
+   :javascript => {
+     :type => "interpreted",
+     :style => [:oo, :functional]
+   },
+   :python => {
+     :type => "interpreted",
+     :style => [:oo]
+   },
+   :java => {
+     :type => "compiled",
+     :style => [:oo]
+   },
+   :clojure => {
+     :type => "compiled",
+     :style => [:functional]
+   },
+   :erlang => {
+     :type => "compiled",
+     :style => [:functional]
+   },
+   :scala => {
+     :type => "compiled",
+     :style => [:functional]
+   }
+ }
+=end
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.collect do |style, hash|
+    hash.collect do |language, language_info|
+
+      new_hash = language
+
+
+      binding.pry
+=begin
+  {
+    language => {language_info => language_info[:type], language_info[:style] => [style]},
+    language => {language_info[:type] => "compiled", language_info[:style] => [style]},
+    language => {language_info[:type] => "compiled", language_info[:style] => [style]}
+  }
+=end
+#  new_hash.to_h
+
+    end
+  end
+#  new_hash
 end
